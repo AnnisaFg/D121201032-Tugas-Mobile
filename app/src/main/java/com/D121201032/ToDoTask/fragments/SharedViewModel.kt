1a38893d86ec642d8b9ee52e6c615b5dd6f0f260
AnnisaFg <annisadarwis123@gmail.com>
2022-12-14 16:15:38 +0800

    upload Project

---
+++ app/src/main/java/com/D121201032/ToDoTask/fragments/SharedViewModel.kt
@@ -0,0 +1,59 @@
+package com.example.todoapp.fragments
+
+import android.app.Application
+import android.graphics.Color.red
+import android.view.View
+import android.widget.AdapterView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.example.todoapp.R
+import com.example.todoapp.data.models.Priority
+import com.example.todoapp.data.models.ToDoData
+
+class SharedViewModel(application: Application): AndroidViewModel(application) {
+
+	val emptyDatabase: MutableLiveData<Boolean> = MutableLiveData(false)
+
+	fun isDatabaseEmpty(toDoData: List<ToDoData>) {
+		emptyDatabase.value = toDoData.isEmpty()
+	}
+
+	val listener: AdapterView.OnItemSelectedListener = object:
+		AdapterView.OnItemSelectedListener {
+			override fun onItemSelected(
+				parent: AdapterView<*>?,
+				view: View?,
+				position: Int,
+				id: Long
+			) {
+				when(position) {
+					0 -> { (parent?.getChildAt(0) as TextView).setTextColor(ContextCompat.getColor(application, R.color.red))}
+					1 -> { (parent?.getChildAt(0) as TextView).setTextColor(ContextCompat.getColor(application, R.color.yellow))}
+					2 -> { (parent?.getChildAt(0) as TextView).setTextColor(ContextCompat.getColor(application, R.color.purple_500))}
+					3 -> { (parent?.getChildAt(0) as TextView).setTextColor(ContextCompat.getColor(application, R.color.green))}
+				}
+			}
+
+		override fun onNothingSelected(
+			parent: AdapterView<*>?
+		) {
+
+		}
+	}
+
+	fun verifyDataFromUser(title: String, desc: String): Boolean {
+		return !(title.isNullOrEmpty() || desc.isNullOrEmpty())
+	}
+
+	fun parsePriorityString(priority: String): Priority {
+		return when(priority) {
+			"Penting Mendesak" -> Priority.High
+			"Penting Tidak Mendesak" -> Priority.Medium
+			"Tidak Penting Mendesak " -> Priority.Normal
+			"Tidak Penting Tidak Mendesak" -> Priority.Low
+			else -> Priority.Normal
+		}
+	}
+}
